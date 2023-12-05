0ffef25fa3aed34dd610af056cb10dad1b25f70c
An Nguyen <an.nguyen@vnetwork.vn>
2023-12-05 14:06:47 +0700

    fix: nextjs hydration error

---
+++ src/components/ClientRender.tsx
@@ -0,0 +1,15 @@
+import { FC, ReactElement, useEffect, useState } from "react";
+
+const ClientRender: FC<{ children: ReactElement }> = ({ children }) => {
+    const [rendered, setRendered] = useState(typeof window !== "undefined");
+
+    useEffect(() => {
+        setRendered(true);
+    }, []);
+
+    if (rendered) return children;
+
+    return <></>;
+};
+
+export default ClientRender;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
 import React from "react"
 import ErrorBoundary from "./components/ErrorBoundary"
 import Player from "./components/Player"
+import ClientRender from './components/ClientRender'
 import { PlayerProps } from "./utils/types"
 
 const VPlayer: React.FC<PlayerProps> = props => {
     return <div id="vnetwork-player">
         <ErrorBoundary>
-            <Player {...props} />
+            <ClientRender>
+                <Player {...props} />
+            </ClientRender>
         </ErrorBoundary>
     </div>
 }
+
+export { type PlayerProps }
 
 export default VPlayer
