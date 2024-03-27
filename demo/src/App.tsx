8b706b6c197e6d7ae4d584d165aeaa812dd0645e
An Nguyen <an.nguyen@vnetwork.vn>
2024-03-27 13:02:36 +0700

    chore: update version demo and hls

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,6 +1,7 @@
 import VPlayer from "vnetwork-player";
 import { FaGithub } from "react-icons/fa";
 import { RiNpmjsFill } from "react-icons/ri";
+import Hls from "hls.js";
 
 import "vnetwork-player/dist/vnetwork-player.min.css";
 
@@ -19,29 +20,12 @@ const App = () => {
           </div>
           <div className="lg:w-[700px] md:w-[600px] aspect-video mt-5">
             <VPlayer
+              Hls={Hls}
               className="w-full h-full"
-              source={[
-                {
-                  label: "360p",
-                  url: "http://res.cloudinary.com/an-nguyen/video/upload/v1701757911/loqq4s3ote6mbwz44kip.mp4",
-                },
-                {
-                  label: "720p",
-                  url: "http://res.cloudinary.com/an-nguyen/video/upload/v1701484115/epekfju1pvusp3xuoq3l.mp4",
-                },
-                {
-                  label: "1080p",
-                  url: "http://res.cloudinary.com/an-nguyen/video/upload/v1701484072/exvk9cxeuhvj70py8dbq.mp4",
-                },
-              ]}
+              source="https://live-par-2-cdn-alt.livepush.io/live/bigbuckbunnyclip/index.m3u8"
               color="#E12F1F"
-              subtitle={[
-                {
-                  lang: "Vietnamese",
-                  url: "/captions.vtt",
-                },
-              ]}
-              autoPlay  
+              autoPlay
+              live
             />
           </div>
 
