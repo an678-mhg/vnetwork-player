2eb40884aabcca9c9ec3c2813121ecfc37f1a868
An Nguyen <an.nq@devtify.com>
2026-05-12 17:16:42 +0700

    fix: update module path aliases in rsbuild configuration

--- docs/rsbuild.config.ts
+++ docs/rsbuild.config.ts
@@ -26,19 +26,19 @@ export default defineConfig({
       resolve: {
         alias: {
           react$: fileURLToPath(
-            new URL('../node_modules/react/index.js', import.meta.url),
+            new URL('./node_modules/react/index.js', import.meta.url),
           ),
           'react/jsx-runtime$': fileURLToPath(
-            new URL('../node_modules/react/jsx-runtime.js', import.meta.url),
+            new URL('./node_modules/react/jsx-runtime.js', import.meta.url),
           ),
           'react/jsx-dev-runtime$': fileURLToPath(
-            new URL('../node_modules/react/jsx-dev-runtime.js', import.meta.url),
+            new URL('./node_modules/react/jsx-dev-runtime.js', import.meta.url),
           ),
           'react-dom$': fileURLToPath(
-            new URL('../node_modules/react-dom/index.js', import.meta.url),
+            new URL('./node_modules/react-dom/index.js', import.meta.url),
           ),
           'react-dom/client$': fileURLToPath(
-            new URL('../node_modules/react-dom/client.js', import.meta.url),
+            new URL('./node_modules/react-dom/client.js', import.meta.url),
           ),
         },
       },
